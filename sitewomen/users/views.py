5ea25359bfaf4c157b7bda814acc69ed290d8405
Rodion <noidoring@gmail.com>
2024-03-01 20:32:53 +0300

    add app users

---
+++ sitewomen/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login_user(request):
+    return HttpResponse('login')
+
+def logout_user(request):
+    return HttpResponse('logout')
