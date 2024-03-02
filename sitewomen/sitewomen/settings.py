5e7a4e81b511b4a4d5cede79f1449df8ff687bda
Rodion <noidoring@gmail.com>
2024-03-02 22:47:26 +0300

    add LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -132,7 +132,11 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
