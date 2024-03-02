da382b91d3e4705fcb4ed2feae03f34614e0bdfa
Rodion <noidoring@gmail.com>
2024-03-02 22:48:33 +0300

    add class LoginUser

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -2,22 +2,32 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import LoginUserForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title':'Авторизация'}
 
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = LoginUserForm()
+    # def get_success_url(self) -> str:
+    #     return reverse_lazy()
+
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'], password=cd['password'])
+
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('home'))
+#     else:
+#         form = LoginUserForm()
     
-    return render(request, 'users/login.html', {'form': form})
+#     return render(request, 'users/login.html', {'form': form})
 
 def logout_user(request):
     logout(request)
