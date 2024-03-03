709d44934da8f4e2bdb83408e2be3b0d68e164c4
Rodion <noidoring@gmail.com>
2024-03-03 11:30:33 +0300

    add register

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
@@ -14,21 +14,23 @@ class LoginUser(LoginView):
     # def get_success_url(self) -> str:
     #     return reverse_lazy()
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'], password=cd['password'])
-
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
-#     else:
-#         form = LoginUserForm()
-    
-#     return render(request, 'users/login.html', {'form': form})
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('users:login'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'users/register.html', {'form':form})
+
+
