0e2b025b25c5284ff06d588959b7e0575a37d689
Rodion <noidoring@gmail.com>
2024-03-06 09:02:24 +0300

    add paths for reset password

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import reverse_lazy
 
 app_name = 'users'
 
@@ -14,4 +16,15 @@ urlpatterns = [
          name='password_change_done'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html',
+                                                      email_template_name='users/password_reset_email.html',
+                                                      success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password-done/<uidb64>/<token>/', 
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', 
+                                          success_url=reverse_lazy('users:password_reset_complete')),
+         name='password_reset_confirm'),
+    path('password-reset/comlete/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete')
 ]
